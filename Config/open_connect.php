9f0c99cc3784b45f41bfaf60d3359aece1e15198
nguyenvantuyen6789 <nguyenvantuyen6789@gmail.com>
2023-06-02 14:03:23 +0700

    Update

--- Config/open_connect.php
+++ Config/open_connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('localhost','root','','highway_project1');
+$connect = mysqli_connect('us-cdbr-east-06.cleardb.net','b26a5ed2fb2d40','45cd74a1','heroku_958b3bd01c82c79');
 mysqli_set_charset($connect,'utf8');
 if(!$connect){
     die("Could not connect to server".mysqli_connect_error());
